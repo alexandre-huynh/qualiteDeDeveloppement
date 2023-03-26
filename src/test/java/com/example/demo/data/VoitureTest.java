d174704588bc07f2060ecf4996ff0b5a89ed0d22
Alexandre Huynh <alexandre8691@gmail.com>
2023-03-27 00:41:50 +0200

    Update VoitureTest.java

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -13,7 +13,7 @@ public class VoitureTest {
         Assert.isInstanceOf(Voiture.class, voiture1, "Pas une voiture");
 
         Assert.isTrue(voiture1.getId() == 0, "Id incorrect");
-        Assert.isTrue(voiture1.getMarque().equals("Renault"), "Marque incorrect");
+        Assert.isTrue(voiture1.getMarque().equals("Renault") == true, "Marque incorrect");
         Assert.isTrue(voiture1.getPrix() == 10000, "Prix incorrect");
 
         voiture1.setId(5);
